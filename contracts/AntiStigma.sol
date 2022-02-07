0ad831651c948ab200c5ce0f9ae3ff7acaa92f5b
Jheave <jimenezjheave123@gmail.com>
2022-02-07 22:55:47 +0800

    created smartcontract logic

--- contracts/AntiStigma.sol
+++ contracts/AntiStigma.sol
@@ -1,41 +1,87 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AntiStigmaSociety is ERC721, ERC721URIStorage, ReentrancyGuard, Ownable {
+contract AntiStigmaSociety is ERC721, Ownable, ReentrancyGuard {
+    using Strings for uint256;
     using Counters for Counters.Counter;
 
-    Counters.Counter private _tokenIdCounter;
-    uint256 public constant ANTI_STIGMA_FREE_COUNT = 10;
+    Counters.Counter private supply;
 
-    constructor() ERC721("Anti-Stigma Society", "$WHOOPIE") {}
+    bool public mintIsActive = false;
+    string public baseURI = "";
+    string public baseExtension = "";
+  
+    uint256 public constant ANTISTIGMASOCIETY_MAX = 10000;
+    uint256 public constant MAX_MINT_TX = 10;
+    uint256 public wlMintPrice = 6 ether;
+    uint256 public publicMintPrice = 12 ether;
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://";
+
+    constructor() ERC721("Anti-Stigma Society", "AntiStigmaSociety") {}
+
+    function totalSupply() public view returns (uint256) {
+        return supply.current();
     }
 
-    function earlyMint() public nonReentrant {
-        // require(mintIsActive, "Minting Anti-Stigma Society is not available yet." );
-		require(_tokenIdCounter.current() < ANTI_STIGMA_FREE_COUNT, "invalid claim");
+    function flipMintState() public onlyOwner {
+        mintIsActive = !mintIsActive;
+    }
+
+    function mint() public payable nonReentrant {
+        require(mintIsActive, "Minting Anti-Stigma Society is not available yet." );
+		require(msg.value >= wlMintPrice, "Anti-Stigma Society: Amount of MATIC sent is incorrect.");
+		_minter(msg.sender);
     }
     
-    // The following functions are overrides required by Solidity.
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
+    function mintForAddress(address _receiver) public onlyOwner {
+        _minter(_receiver);
+    }
+  
+    function walletOfOwner(address _owner) public view returns (uint256[] memory) {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory ownedTokenIds = new uint256[](ownerTokenCount);
+        uint256 currentTokenId = 1;
+        uint256 ownedTokenIndex = 0;
+        while (ownedTokenIndex < ownerTokenCount && currentTokenId <= ANTISTIGMASOCIETY_MAX) {
+            address currentTokenOwner = ownerOf(currentTokenId);
+    
+            if (currentTokenOwner == _owner) {
+                ownedTokenIds[ownedTokenIndex] = currentTokenId;
+                ownedTokenIndex++;
+            }
+            currentTokenId++;
+        }
+        return ownedTokenIds;
+    }
+    
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
+    function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+        baseExtension = _newBaseExtension;
     }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId),"ERC721Metadata: URI query for nonexistent token");
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), baseExtension)) : "";
+    }
+
+    function withdraw() public onlyOwner {
+      (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+      require(os);
+    }
+    
+    function _minter(address _receiver) internal {
+        for (uint256 i = 0; i < i; i++) {
+            supply.increment();
+            _safeMint(_receiver, supply.current());
+        }
+    }
+    
 }
