16c917e969d482903eaf1476abbfee56df47a447
Walter Santiago <waltersantiago@UBVPNN3013.local>
2022-03-07 00:59:57 +0800

    This commit will add framer-motion

--- components/pages/agents/AgentsSection.js
+++ components/pages/agents/AgentsSection.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 
 import styled from "styled-components";
+import { motion } from "framer-motion"
 
-const AgentList = styled.section`
+const AgentList = styled(motion.section)`
     background-image:url(${({ background }) => background});
-
+    overflow-x: hidden;
+    
     article {
         position:relative;
         z-index:9;
@@ -30,9 +32,49 @@ const AgentList = styled.section`
     }
 `
 
+const agentListVariant = {
+    hidden: { opacity: 0 },
+    visible: {
+        opacity: 1,
+        transition: {
+            when: "beforeChildren"
+        }
+    },
+}
+
+const doodleVariant = (direction) => {
+    return {
+        hidden: {
+            opacity: 0,
+            x: direction ? '-50vw' : '50vw'
+        },
+        visible: {
+            opacity: 1,
+            x: direction ? '0' : '0',
+            transition: {
+                duration: 0.8,
+            }
+        },
+    }
+}
+
+const swingingVariant = {
+    swing: {
+        rotate: [3, -3],
+        transition: {
+            yoyo: Infinity
+        }
+    }
+}
+
 const AgentsSection = ({ headerBg, smIcon, background, charImg, text, right, }) => {
     return (
-        <AgentList className='position-relative'>
+        <AgentList
+            className='position-relative'
+            variants={agentListVariant}
+            initial='hidden'
+            animate='visible'
+        >
             <Image
                 alt='headerBg'
                 src={background}
@@ -40,7 +82,16 @@ const AgentsSection = ({ headerBg, smIcon, background, charImg, text, right, }) 
                 objectFit="cover"
                 quality={100}
             />
-            <article className={`d-flex container ${right ? 'justify-content-end section-end' : 'section-start'}`}>
+            <motion.article
+                className={`d-flex container ${right ? 'justify-content-end section-end' : 'section-start'}`}
+                variants={agentListVariant}
+                initial='hidden'
+                whileInView='visible'
+                viewport={{
+                    once: true,
+                    margin: '-300px'
+                }}
+            >
                 <div className='agent-list__logo-icon'>
                     <Image
                         alt='smIcon'
@@ -49,26 +100,36 @@ const AgentsSection = ({ headerBg, smIcon, background, charImg, text, right, }) 
                         height={80}
                     />
                 </div>
-                <div className={`col-md-7 d-flex ${!right ? 'order-2' : ''}`}>
+                <motion.div
+                    className={`col-md-7 d-flex ${!right ? 'order-2' : ''}`}
+                    variants={doodleVariant(right)}
+                >
                     <Image
                         alt='charImg'
                         src={charImg}
                         width={710}
                         height={400}
                     />
-                </div>
-                <div className='agent-list__content col-md-5 py-3'>
-                    <div className='text-center mb-3'>
+                </motion.div>
+                <motion.div
+                    className='agent-list__content col-md-5 py-3'
+                    variants={doodleVariant(!right)}
+                >
+                    <motion.div
+                        className='text-center mb-3'
+                        variants={swingingVariant}
+                        animate='swing'
+                    >
                         <Image
                             alt='headerBg'
                             src={headerBg}
                             width={300}
                             height={150}
                         />
-                    </div>
+                    </motion.div>
                     <p>{text}</p>
-                </div>
-            </article>
+                </motion.div>
+            </motion.article>
         </AgentList>
     );
 };
