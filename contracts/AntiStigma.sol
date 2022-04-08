c6963b1104949eb008200c7851f58d47b85709d4
jheave <jimenezjheave123@gmail.com>
2022-04-08 23:32:54 +0800

    refractor

--- contracts/AntiStigma.sol
+++ contracts/AntiStigma.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+
 contract AntiStigmaSociety is ERC721, Ownable, ReentrancyGuard {
     using Strings for uint256;
     using Counters for Counters.Counter;
@@ -14,13 +15,13 @@ contract AntiStigmaSociety is ERC721, Ownable, ReentrancyGuard {
 
     bool public saleOpen = false;
     bool public reveal = false; // reveal is true when presale is over and sale is open
-    bool public onlyWhitelisted = true;
+    bool public whitelistMint = true;
 
     address[] public whitelistedAddresses;
 
     string public baseURI = "";
     string public baseExtension = ".json";
-  
+
     uint256 public constant ANTISTIGMASOCIETY_MAX = 8750;
     uint256 public constant OWNER_MAX_CLAIM = 750;
     uint256 public constant MAX_MINT_TX = 10;
@@ -30,9 +31,10 @@ contract AntiStigmaSociety is ERC721, Ownable, ReentrancyGuard {
 
     constructor() ERC721("Anti-Stigma Society - Alpha Class", "AntiStigmaSociety") {}
 
-    modifier mintCompliance(uint256 _mintAmount) {
-        require(_mintAmount > 0 && _mintAmount <= MAX_MINT_TX, "Invalid mint amount!");
-        require(supply.current() + _mintAmount <= maxSupply, "Max supply exceeded!");
+    modifier mintCondition(uint256 _amount) {
+        require(_amount > 0 && _amount <= MAX_MINT_TX, "Invalid mint amount!");
+        require(supply.current() < 8000, "invalid claim");
+        require(supply.current() + _amount <= ANTISTIGMASOCIETY_MAX, "Max supply exceeded!");
         _;
     }
 
@@ -40,18 +42,33 @@ contract AntiStigmaSociety is ERC721, Ownable, ReentrancyGuard {
         return supply.current();
     }
 
-    function mint(uint256 amount) public payable nonReentrant {
+    function mint(uint256 _amount) public payable mintCondition(_amount) nonReentrant {
         require(saleOpen == true, 'Minting is not open');
-        require(supply.current() < 8000, "invalid claim");
-		require(msg.value >= wlMintPrice * amount, "Anti-Stigma Society: Amount of MATIC sent is incorrect.");
-		_minter(msg.sender, amount);
+        if (whitelistMint == true) {
+            require(isWhitelisted(msg.sender), "this address is not whitelisted");
+            //TODO: create isWhitelisted logic
+            require(msg.value >= wlMintPrice * _amount, "Anti-Stigma Society: Amount of MATIC sent is incorrect.");
+            _minter(msg.sender, _amount);
+        }
+        require(msg.value >= publicMintPrice * _amount, "Anti-Stigma Society: Amount of MATIC sent is incorrect.");
+        //TODO: refractor this to error()
+        _minter(msg.sender, _amount);
     }
-    
+
     function ownerClaim(address _receiver, uint256 amount) public onlyOwner {
         require((supply.current() < OWNER_MAX_CLAIM) && (supply.current() < ANTISTIGMASOCIETY_MAX), "Max supply exceeded!");
         _minter(_receiver, amount);
     }
-  
+
+    function isWhitelisted(address _user) public view returns (bool) {
+        for (uint i = 0; i < whitelistedAddresses.length; i++) {
+            if (whitelistedAddresses[i] == _user) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     function walletOfOwner(address _owner) public view returns (uint256[] memory) {
         uint256 ownerTokenCount = balanceOf(_owner);
         uint256[] memory ownedTokenIds = new uint256[](ownerTokenCount);
@@ -59,7 +76,7 @@ contract AntiStigmaSociety is ERC721, Ownable, ReentrancyGuard {
         uint256 ownedTokenIndex = 0;
         while (ownedTokenIndex < ownerTokenCount && currentTokenId <= ANTISTIGMASOCIETY_MAX) {
             address currentTokenOwner = ownerOf(currentTokenId);
-    
+
             if (currentTokenOwner == _owner) {
                 ownedTokenIds[ownedTokenIndex] = currentTokenId;
                 ownedTokenIndex++;
@@ -68,7 +85,7 @@ contract AntiStigmaSociety is ERC721, Ownable, ReentrancyGuard {
         }
         return ownedTokenIds;
     }
-    
+
     function setBaseURI(string memory _newBaseURI) public onlyOwner {
         baseURI = _newBaseURI;
     }
@@ -78,7 +95,7 @@ contract AntiStigmaSociety is ERC721, Ownable, ReentrancyGuard {
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        require(_exists(tokenId),"ERC721Metadata: URI query for nonexistent token");
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
         return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), baseExtension)) : "";
     }
 
@@ -96,15 +113,14 @@ contract AntiStigmaSociety is ERC721, Ownable, ReentrancyGuard {
     }
 
     function withdraw() public onlyOwner {
-      (bool os, ) = payable(owner()).call{value: address(this).balance}("");
-      require(os);
+        payable(owner()).transfer(address(this).balance);
     }
-    
+
     function _minter(address _receiver, uint256 amount) internal {
         for (uint256 i = 0; i < amount; i++) {
             supply.increment();
             _safeMint(_receiver, supply.current());
         }
     }
-    
+
 }
